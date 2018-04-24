35217bac2e22a29c709181c2e68d3c7942f7d225
FernandoCaballero <fer.caballero21@gmail.com>
2018-04-24 10:33:42 -0300

    Agrego documentacion y notas

--- Ejemplo_01.hs
+++ Ejemplo_01.hs
@@ -1,18 +1,35 @@
+
+--Las variables son incognitas no espacios de memoria
+--f(x)=x*2
 doble numero = numero * 2 
+
 aprobo nota = nota >= 6
-edadSeniorBurns = 120
+
+--notacion infija para operadores aritmeticos y de comparacion
+-- 2+3 , 6/3 , 5 == 5
+-- 2 + 3 --> (+) 2 3 conversion a notacion prefija
+
+--Constanes
+edadSeniorBurns = 120 
 segundoNombre = "jay"
+
+--Guardas, no ifs
+--Pipelines alineados
 max x y | x > y = x
         | otherwise = y
 min a b | a < b = a
         | otherwise = b
 
-cuadruple = doble.doble
+--Composicion
+--cuadruple numero = (doble.doble) numero
+cuadruple = doble.doble -- simplifico los parametros y saco los parentesis
 
-longitudPar :: [Char] -> Bool
+longitudPar :: [Char] -> Bool --Definicion de tipo de dato de funcion longitudPar
 longitudPar = even.length
------------------------------------
-type Persona = (String,Integer,String)
+------------------------------------------------------------------------------------
+--Defino un tipo de datos
+--Tupla-> tipo de datos compusto, con elementos relacionados
+type Persona = (String,Integer,String) --tupla de 3 elemntos
 
 laura :: Persona
 laura = ("Laura",32,"Av. Libertador 747")
@@ -25,6 +42,12 @@ mayor18 ed = ed > 18
 
 esMayor :: Persona -> Bool
 esMayor = mayor18.edad
+
+--Main> (not.esMayor) laura /// Main> (not.mayor18.edad) laura /// Puedo componer mientras que la imagen de mi funcion sea del mismo tipo que el domino de la proxima funcion
+
+------------------------------------------------------------------------------------
+
+
 
 
 
